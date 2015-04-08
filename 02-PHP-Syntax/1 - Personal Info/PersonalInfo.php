6d7de99b3b97a62d386514380e028959782e889c
Svetlozar Kirkov <svetlozark@gmail.com>
2015-04-08 20:43:28 +0300

    initial

---
+++ 02-PHP-Syntax/1 - Personal Info/PersonalInfo.php
@@ -0,0 +1,6 @@
+<?php
+    $firstName = 'Bill';
+    $lastName = 'Gates';
+    $age = 59;
+    $fullInfo = 'My name is ' . $firstName . ' ' . $lastName . ' and I am ' . $age . ' years old.';
+    echo $fullInfo;

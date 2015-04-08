8eae0e65717cb4478cceb10a87e415cf0321ad40
Svetlozar Kirkov <svetlozark@gmail.com>
2015-04-09 02:34:59 +0300

    fix

--- 02-PHP-Syntax/2 - Sum Two Numbers/SumTwoNumbers.php
+++ 02-PHP-Syntax/2 - Sum Two Numbers/SumTwoNumbers.php
@@ -1,14 +1,14 @@
 <?php
-    function printResult($firstNumber, $secondNumber) {
-        $sum = number_format((float)round($firstNumber + $secondNumber, 2), 2, '.', '');
-        echo '$firstNumber + $secondNumber = ' . $firstNumber . ' + ' . $secondNumber . ' = ' . $sum . '<br>';
-    }
-    $firstNumber = 2;
-    $secondNumber = 5;
-    printResult($firstNumber, $secondNumber);
-    $firstNumber = 1.567808;
-    $secondNumber = 0.356;
-    printResult($firstNumber, $secondNumber);
-    $firstNumber = 1234.5678;
-    $secondNumber = 333;
-    printResult($firstNumber, $secondNumber);
+function printResult($firstNumber, $secondNumber) {
+    $sum = number_format((float)round($firstNumber + $secondNumber, 2), 2, '.', '');
+    echo '$firstNumber + $secondNumber = ' . $firstNumber . ' + ' . $secondNumber . ' = ' . $sum . '<br>';
+}
+$firstNumber = 2;
+$secondNumber = 5;
+printResult($firstNumber, $secondNumber);
+$firstNumber = 1.567808;
+$secondNumber = 0.356;
+printResult($firstNumber, $secondNumber);
+$firstNumber = 1234.5678;
+$secondNumber = 333;
+printResult($firstNumber, $secondNumber);
--- 02-PHP-Syntax/3 - Dump Variable/DumpVariable.php
+++ 02-PHP-Syntax/3 - Dump Variable/DumpVariable.php
@@ -1,14 +1,14 @@
 <?php
-    function printResult($inputVar) {
-        if (is_numeric($inputVar) === true) {
-            echo var_dump($inputVar) . '<br>';
-        }
-        else {
-            echo gettype ($inputVar) . '<br>';
-        }
+function printResult($inputVar) {
+    if (is_numeric($inputVar) === true) {
+        echo var_dump($inputVar) . '<br>';
     }
-    printResult('hello');
-    printResult(15);
-    printResult(1.234);
-    printResult(array(1,2,3));
-    printResult((object)[2,34]);
+    else {
+        echo gettype ($inputVar) . '<br>';
+    }
+}
+printResult('hello');
+printResult(15);
+printResult(1.234);
+printResult(array(1,2,3));
+printResult((object)[2,34]);

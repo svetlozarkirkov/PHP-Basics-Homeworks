4860bee3b4914ade138dcca10a1ddd073bd1eb50
Svetlozar Kirkov <svetlozark@gmail.com>
2015-04-13 10:47:52 +0300

    completed 6 and 7

--- 02-PHP-Syntax/6 - HTML Table/InformationTable.php
+++ 02-PHP-Syntax/6 - HTML Table/InformationTable.php
@@ -1,1 +1,21 @@
-<?php
+<html>
+<head>
+    <title>Information Table</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <?php
+        $info = array(
+                    "Name"=>"Gosho",
+                    "Phone number"=>"0882-321-423",
+                    "Age"=>"24",
+                    "Address"=>"Hadji Dimitar"
+        );
+        echo '<table cellspacing="0"><tbody>';
+        foreach($info as $x => $x_value) {
+            echo '<tr><th>' . $x . '</th><td>' . $x_value . '</td></tr>';
+        }
+        echo '</tbody>';
+    ?>
+</head>
+<body>
+</body>
+</html>
--- 02-PHP-Syntax/7 - Form Data/GetFormData.php
+++ 02-PHP-Syntax/7 - Form Data/GetFormData.php
@@ -1,1 +1,23 @@
-<?php
+<html>
+<head>
+    <title>Get Form Data</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <form action="" method="post">
+        <fieldset>
+            <input type="text" name="name" placeholder="Name..."><br>
+            <input type="text" name="age" placeholder="Age..."><br>
+            <input type="radio" name="sex" value="male" checked>Male<br>
+            <input type="radio" name="sex" value="female">Female<br>
+            <input type="submit" name="submit"><br>
+        </fieldset>
+    </form>
+    <?php
+        if($_POST) {
+            echo 'My name is ' . $_POST['name'] . ".\n";
+            echo 'I am ' . (int)$_POST['age'] . ' years old. I am ' . htmlspecialchars($_POST['sex']) . ".";
+        }
+    ?>
+</body>
+</html>
